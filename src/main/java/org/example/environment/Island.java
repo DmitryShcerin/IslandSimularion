d5ab49130d0d7aa354de547b7b2c1e1bfef32677
DmitryShcerin <shcherin95@mail.ru>
2025-10-15 10:43:17 +0300

    fixed an issue, now project finished

--- src/main/java/org/example/environment/Island.java
+++ src/main/java/org/example/environment/Island.java
@@ -63,7 +63,7 @@ public class Island {
     }
 
     public Location getLocation(int x, int y) {
-        return (x <= 0 && x < width && y >= 0 && y < height) ? locations[x][y] : null;
+        return (x >= 0 && x < width && y >= 0 && y < height) ? locations[x][y] : null;
     }
 
     public int getWidth() {
